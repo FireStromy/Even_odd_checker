60f8fa4420497d21eeb08d16a3ff493049b2b42a
Tejas K <103427612+FireStromy@users.noreply.github.com>
2023-08-20 19:58:36 +0530

    updated file

--- Even or odd checker.py
+++ Even or odd checker.py
@@ -5,11 +5,14 @@
 # Solution
 
 print("*** Welcome to Even or odd checker *** \n")
+#Asking user to input the number to be checked
 num = float(input("Which natural number do you want to check?\n Number: "))
 number = int(round(num))
 
 result = number % 2
 print()
+
+#printing the result
 if result == 0:
     print(f"Answer: '{number}' is an even number.")
 else:
